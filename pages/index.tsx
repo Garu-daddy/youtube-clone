e0b9d9e54a527fbaccffdbefe9320922670c1b7c
Yeonseung Yoo <85042907+Garu-daddy@users.noreply.github.com>
2022-08-21 03:44:18 +0900

    feat: hid API KEY and added map function for btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { useEffect, useState } from "react";
 import IndexBtn from "../components/Button";
 import Header from "../components/core/Header";
 import Seo from "../components/Seo";
 
-const Home: NextPage = () => {
-  const [videos, setVideos] = useState([]);
+const categories = [
+  "전체",
+  "실시간 맞춤 동영상",
+  "야구",
+  "축구",
+  "게임",
+  "실시간 프로그램",
+  "액션 어드벤처",
+  "예능",
+  "영화 프로그램",
+];
 
-  // useEffect(() => {
-  //   getVideos().then((data) => setVideos(data));
-  // }, []);
+type Video = {
+  id: string;
+  snippet: {
+    title: string;
+    channelTitle: string;
+  };
+};
+
+const Home: NextPage = () => {
+  const [videos, setVideos] = useState<Video[]>([]);
 
   useEffect(() => {
     (async () => {
       const { items } = await (
         await fetch(
-          `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAaoApWq1cJg_w5_BBUI6bbVvzXnBl7y-8`
+          `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`
         )
       ).json();
-      console.log(items);
       setVideos(items);
     })();
   }, []);
@@ -29,17 +43,11 @@ const Home: NextPage = () => {
       <Seo title="Home" />
       <Header />
       <section className="categories bg-zinc-900 justify-around w-8/12 flex m-auto">
-        <IndexBtn category="전체" />
-        <IndexBtn category="실시간 프로그램" />
-        <IndexBtn category="야구" />
-        <IndexBtn category="축구" />
-        <IndexBtn category="스포츠" />
-        <IndexBtn category="게임" />
-        <IndexBtn category="최근 업로드 영상" />
-        <IndexBtn category="요리" />
-        <IndexBtn category="영화 프로그램" />
-        <IndexBtn category="맞춤 동영상" />
+        {categories.map((category, index) => (
+          <IndexBtn key={`category-${index}`} category={category} />
+        ))}
       </section>
+
       <main className="bg-zinc-900 text-zinc-300 px-48 py-6 flex flex-wrap m-auto">
         {videos.map((video) => (
           <div className="w-1/3 mb-3" key={video.id}>
