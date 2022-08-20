f7e281ce37e5de6d53b246082f6ea53ed4c0dfc6
Yeonseung Yoo <85042907+Garu-daddy@users.noreply.github.com>
2022-08-20 14:47:30 +0900

    added useEffect and useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 import Header from "../components/core/Header";
 import Seo from "../components/Seo";
 
 const Home: NextPage = () => {
+  const [videos, setVideos] = useState([]);
+
+  // useEffect(() => {
+  //   getVideos().then((data) => setVideos(data));
+  // }, []);
+
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAaoApWq1cJg_w5_BBUI6bbVvzXnBl7y-8`
+        )
+      ).json();
+      console.log(data);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
