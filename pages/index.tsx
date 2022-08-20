96eddd56bd5063eeea2f9a80869c86ba23172104
Yeonseung Yoo <85042907+Garu-daddy@users.noreply.github.com>
2022-08-20 16:43:21 +0900

    youtube data api fetched

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/core/Header";
 import Seo from "../components/Seo";
 
@@ -12,18 +12,35 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     (async () => {
-      const data = await (
+      const { items } = await (
         await fetch(
           `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAaoApWq1cJg_w5_BBUI6bbVvzXnBl7y-8`
         )
       ).json();
-      console.log(data);
+      console.log(items);
+      setVideos(items);
     })();
   }, []);
+
   return (
     <div>
       <Seo title="Home" />
       <Header />
+      <body className="bg-zinc-900 text-zinc-300">
+        {videos.map((video) => (
+          <div key={video.id}>
+            <div className="w-96 overflow-hidden">
+              <img
+                className="hover:scale-110 ease-in-out duration-300"
+                src={`http://img.youtube.com/vi/${video.id}/mqdefault.jpg`}
+                alt="video thumbnail"
+              />
+            </div>
+            <h3>{video.snippet.title}</h3>
+            <h2 className="text-zinc-500">{video.snippet.channelTitle}</h2>
+          </div>
+        ))}
+      </body>
     </div>
   );
 };
