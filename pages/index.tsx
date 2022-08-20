bac8e52dbc311fe70519a26513cd1dee2343539d
Yeonseung Yoo <85042907+Garu-daddy@users.noreply.github.com>
2022-08-20 23:24:12 +0900

    feat: added Button components into index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useEffect, useState } from "react";
+import IndexBtn from "../components/Button";
 import Header from "../components/core/Header";
 import Seo from "../components/Seo";
 
@@ -23,12 +25,24 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <Seo title="Home" />
       <Header />
-      <body className="bg-zinc-900 text-zinc-300">
+      <section className="categories bg-zinc-900 justify-around w-8/12 flex m-auto">
+        <IndexBtn category="전체" />
+        <IndexBtn category="실시간 프로그램" />
+        <IndexBtn category="야구" />
+        <IndexBtn category="축구" />
+        <IndexBtn category="스포츠" />
+        <IndexBtn category="게임" />
+        <IndexBtn category="최근 업로드 영상" />
+        <IndexBtn category="요리" />
+        <IndexBtn category="영화 프로그램" />
+        <IndexBtn category="맞춤 동영상" />
+      </section>
+      <main className="bg-zinc-900 text-zinc-300 px-48 py-6 flex flex-wrap m-auto">
         {videos.map((video) => (
-          <div key={video.id}>
+          <div className="w-1/3 mb-3" key={video.id}>
             <div className="w-96 overflow-hidden">
               <img
                 className="hover:scale-110 ease-in-out duration-300"
@@ -36,12 +50,12 @@ const Home: NextPage = () => {
                 alt="video thumbnail"
               />
             </div>
-            <h3>{video.snippet.title}</h3>
+            <h3 className="overflow-hidden">{video.snippet.title}</h3>
             <h2 className="text-zinc-500">{video.snippet.channelTitle}</h2>
           </div>
         ))}
-      </body>
-    </div>
+      </main>
+    </>
   );
 };
 
